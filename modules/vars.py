0317e813fdbf3feba83ea9db3568a6ec4b710c27
piyush4014 <140847217+piyush4014@users.noreply.github.com>
2023-12-29 20:43:14 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
 api_id = "11454995"
 api_hash = "78718c5bffa26f1a2fcdd968ce057a58"
-bot_token = "6741932853:AAFeevHkM53ww6Itu26JwFa8ODpLGeCrDk4"
+bot_token = "6574193536:AAGFp23slEq6jIgV7dmhP6sW1SQlG-_ADR0"
